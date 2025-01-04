aad3f7bfcb1f5588e59738127d54f63abd0fa313
Roshnipriya <roshnipriyagr.22msc@kongu.edu>
2025-01-04 12:00:19 +0530

    Login

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Amsam Textiles Login',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
