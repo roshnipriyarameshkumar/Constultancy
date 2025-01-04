1bad34ab8d07dfa765d9cf5c9956b360c216486a
Roshnipriya <roshnipriyagr.22msc@kongu.edu>
2025-01-04 12:05:16 +0530

    Login

--- lib/login.dart
+++ lib/login.dart
@@ -31,7 +31,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Amsam Textiles Login'),
+        title: const Text('Amsam Textiles Login!'),
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
