9184c11174db2befd7784ff5e85fea05b1c3b67d
yukeshxxd <yukeshv.22msc@kongu.edu>
2025-04-02 21:37:15 +0530

    database connectivity

--- lib/admin.dart
+++ lib/admin.dart
@@ -192,16 +192,7 @@ class _AdminPageState extends State<AdminPage> {
             ListTile(title: Text('Profile'), onTap: () {}),
             ListTile(title: Text('Sales Insights'), onTap: () {}),
             ListTile(title: Text('More'), onTap: () {}),
-            ListTile(
-              title: Text('Sign Out'),
-              onTap: () async {
-                await FirebaseAuth.instance.signOut();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => LoginPage()),
-                );
-              },
-            ),
+            ListTile(title: Text('Sign Out'), onTap: () {}),
           ],
         ),
       ),
